4412aad0e0a0ba9a23367f8da16f02c567d1ac0a
Zair65 <uhayat365@hotmail.co.uk>
2019-02-02 21:59:11 +0000

    Made changes to Port configs-errors

---
+++ src/DeleteGod.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import Home from './Home.js';
+import axios from 'axios';
+import { Button} from 'reactstrap';
+
+class DeleteAccount extends React.Component {
+  constructor() {
+    super();
+  this.state = { Gods: [], 
+                God :null,
+                GodType:null,
+                Pantheon:null
+
+};
+
+  }
+
+  handleDelete = (id) => {
+
+            axios({
+                method: "delete",
+                url: "http://localhost:8080/Smite/api/SmitePath/DeleteAccount/" + id,   
+        responseType: "json"
+        }).then(response => {
+          this.props.history.push('/Home');
+        })
+    
+  }
+
+
+handleAllAccount = () => {
+
+            axios({
+                method: "get",
+                url: "http://localhost:8080/Smite/api/SmitePath/getAllAccounts",   
+        responseType: "json"
+        }).then(response => {
+            console.log(response);
+            let accounts = response.data;
+            for(let account=0; account<accounts.length; account++){
+                if(accounts[account].UserName === this.state.UserName && accounts[account].Password===this.state.Password && accounts[account].Email===this.state.Email){
+                    console.log("match");
+                    this.setState({
+                        Account:accounts[account]
+                    });
+                    this.handleDelete(this.state.Account.id);
+                }
+
+
+            }
+        })
+    
+  }
+
+
+ handleUserNameChange = (event) => {
+    this.setState({UserName: event.target.value});
+  }
+  handleEmailChange = (event) => {
+    this.setState({Email: event.target.value});
+  }
+  handlePasswordChange = (event) => {
+    this.setState({Password: event.target.value});
+  }
+
+
+  render () {
+    
+    return (
+      <div className="Everything Account_login">
+  <Home/>
+<div class="container">
+  <h1>Enter details to delete Account</h1>
+  
+ <form action="" id="join-us">
+    <div class="fields">
+      <span>
+       <input placeholder="Name" type="text" defaultValue={this.state.UserName} onChange={this.handleUserNameChange} />
+    </span>
+    <br />
+     <span>
+       <input placeholder="Password" type="password" defaultValue={this.state.Password} onChange={this.handlePasswordChange} />
+    </span>
+    <br />
+     <span>
+       <input placeholder="Email" type="email" defaultValue={this.state.Email} onChange={this.handleEmailChange} />
+    </span>
+    </div>
+    <Button className="DeleteAccButton" onClick={this.handleAllAccount}>Delete Account</Button>
+  </form>
+</div>
+    </div>
+    );
+  }
+
+}
+export default DeleteAccount; 
